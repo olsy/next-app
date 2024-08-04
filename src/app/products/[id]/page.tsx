e296a0e1c27ded0897a481d910b030511fedba82
oleksandr <oleksandr.sylko@gmail.com>
2024-08-04 13:36:06 +0300

    Update Eslint config; Format code

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,35 +1,42 @@
-import Link from 'next/link'
-import {Product} from '../types'
-import Card from "@/components/Card";
-import {revalidatePath} from "next/cache";
+import Link from 'next/link';
+import { Product } from '../types';
+import Card from '@/components/Card';
+import { revalidatePath } from 'next/cache';
 
-// export const revalidate = 60; // in milliseconds
+export const revalidate = 60; // in seconds
 
-const fetchProduct = async (id:string): Promise<Product | null> => {
-    const res = await fetch(`${process.env.API_URL}/${id}`);
+const fetchProduct = async (id: string): Promise<Product | null> => {
+  const res = await fetch(`${process.env.API_URL}/${id}`);
 
-    if(!res.ok){
-        revalidatePath('/products')
-        return null;
-    }
+  if (!res.ok) {
+    revalidatePath('/products');
+    return null;
+  }
 
-    return await res.json()
-}
+  return await res.json();
+};
 
-export default async function ProductPage({params}:any) {
-    const product = await fetchProduct(params.id)
+export default async function ProductPage({ params }: any) {
+  const product = await fetchProduct(params.id);
 
-    if(!product){
-        return <main className="flex min-h-screen flex-col items-center p-24 gap-4">
-            <h4>Sorry, looks like this page does not exist any more!</h4>
-            <span>Please, back to <a href="/products" className="text-red-500">products</a></span>
-        </main>
-    }
-
+  if (!product) {
     return (
-        <main className="flex min-h-screen flex-col items-center p-24 gap-4">
-            <Card {...product} />
-            <Link href="/products">back</Link>
-        </main>
+      <main className='flex min-h-screen flex-col items-center p-24 gap-4'>
+        <h4>Sorry, looks like this page does not exist any more!</h4>
+        <span>
+          Please, back to{' '}
+          <a href='/products' className='text-red-500'>
+            products
+          </a>
+        </span>
+      </main>
     );
+  }
+
+  return (
+    <main className='flex min-h-screen flex-col items-center p-24 gap-4'>
+      <Card {...product} />
+      <Link href='/products'>back</Link>
+    </main>
+  );
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,53 +1,28 @@
-import Link from 'next/link'
-import {Product} from "@/app/products/types";
-import Card from "@/components/Card";
+import Link from 'next/link';
+import { Product } from '@/app/products/types';
+import Card from '@/components/Card';
 
-// export const revalidate = 60; // in milliseconds
-
-// export async function getServerSideProps(ctx) {
-//     const {req, res} = ctx
-//
-//     const authHeader = req.headers.get('authorization');
-//
-//     if (!authHeader) {
-//         res.setHeader("WWW-Authenticate", 'Basic realm="Protected"')
-//         res.statusCode = 401
-//         res.end("Unauthorized")
-//     }
-//
-//     const credentials = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':')
-//
-//     console.log(authHeader, credentials)
-//
-//     const [user, pass] = credentials;
-//
-//     if (!(user == 'admin' && pass == 'pass')) {
-//         res.setHeader("WWW-Authenticate", 'Basic realm="Protected"')
-//         res.statusCode = 401
-//         res.end("Unauthorized")
-//     }
-//
-//     // return NextResponse.next();
-//
-//     return {
-//         props: {}
-//     }
-// }
+export const revalidate = 60; // in seconds
 
 const fetchProducts = async (): Promise<Product[]> => {
-    const res = await fetch(process.env.API_URL as string);
-    return await res.json()
-}
+  const res = await fetch(process.env.API_URL as string);
+  return await res.json();
+};
 
-export default async function Products() {
-    const products = await fetchProducts()
+export default async function ProductsPage() {
+  const products = await fetchProducts();
 
-    return (
-        <main className="flex min-h-screen flex-col items-center p-24 gap-4">
-            {products.map((product) =>
-                <Link key={product.id} href={`/products/${product.id}`} className="w-full">
-                    <Card {...product}/>
-                </Link>)}
-        </main>
-    );
+  return (
+    <main className='flex min-h-screen flex-col items-center p-24 gap-4'>
+      {products.map(product => (
+        <Link
+          key={product.id}
+          href={`/products/${product.id}`}
+          className='w-full'
+        >
+          <Card {...product} />
+        </Link>
+      ))}
+    </main>
+  );
 }
